f8bdf25f6042e365772a2c79624528da6a9908ab
Elijah Prom <elijahprom@gmail.com>
2021-04-27 15:18:56 -0700

    couldnt write a test for it

--- javascript/linked-list/lib/linkedlist.js
+++ javascript/linked-list/lib/linkedlist.js
@@ -1,90 +1,121 @@
-"use strict";
+/* eslint-disable indent */
+'use strict';
 
-const Node = require("./node.js");
+const Node = require('./node.js');
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.size = 0;
-  }
+class List {
+	constructor() {
+		this.head = null;
+	}
 
-  append(value) {
-    let current = this.head;
-    let newNode = new Node(value);
-      this.head = newNode;
-      return this;
-    } else {
-    while(current.next) {
-      current = current.next;
-    }
-    current.next = newNode;
-    return this;
-  }
+	append(value) {
+		let node = new Node(value);
 
-  insertBefore(value, newVal) {
-    let current = this.head;
-    let newNode = new Node(newVal);
-  if (!this.includes(value)) {
-    return false;
-  }else {
-    while(current.next.value !== value) {
-      current = current.next;
-    }
-    newNode.next = current.next;
-    current.next = newNode;
+		// This happens if the list is empty
+		if (!this.head) {
+			this.head = node;
+			return this;
+		}
 
-    return this;
+		// If we have stuff, add it to the end
+		let currentNode = this.head;
+		while (currentNode.next) {
+			currentNode = currentNode.next;
+		}
 
-  }
-  }
+		currentNode.next = node;
+		//
+		return this;
+	}
 
-  insertAfter(value, newVal) {
-    let current = this.head;
-    let newNode = new Node(newVal);
-    let nextNode = null;
+	includes(value) {
+		//if theres no head just return false cuz their aint no thang in here.
+		if (!this.head) {
+			return false;
+		}
+		let current = this.head;
 
-    while(current.value !== value) {
-      current = current.next;
-      nextNode = current.next;
-    }
+		while (current.next) {
+			if (current.value === value) {
+				return true;
+			}
+			current = current.next;
+		}
 
-    current.next = newNode;
-    newNode.next = nextNode;
+		if (current.value === value) {
+			return true;
+		}
+		return false;
+	}
 
-    return this;
-  }
+	insert(value) {
+		let node = new Node(value);
 
-  includes(value) {
-    let thisNode =this.head;//this node means this.head
-    while(thisNode) {
-      if (thisNode.value === value) {//if the value is in there then return true
-        return true;
-      }
-      thisNode = thisNode.next;//
-    }
-    return false;//return false if you cant find it
-  }
+		node.next = this.head;
 
-  insert(value) {
-  let node = new Node(value);
+		this.head = node;
 
-  node.next = this.head;
+		return this;
+	}
 
-  this.head = node;
+	insertAfter(value, newVal) {
+		let node = new Node(newVal);
+		let current = this.head;
 
-  return this;
-  }
+		while (current.value !== value) {
+			current = current.next;
+		}
 
-  toString() {
-    let data = '';//string to save data from our node
-    let current = this.header;//created current to start at the header
-    while(current != null ){// well keep going until current is equal to null if current is null then there are no nodes left in the list
-      data = data + current.data + ""; //saves the data at current node in a string
-      current = current.next;//now we go to the next node
-    }
-    return data;// now we return the data
-  }
+		node.next = current.next;
+		current.next = node;
+
+		return this;
+	}
+
+	insertBefore(value, newVal) {
+		let node = new Node(newVal);
+		let current = this.head;
+
+		while (current.next.value !== value) {
+			current = current.next;
+		}
+
+		node.next = current.next;
+		current.next = node;
+
+		return this;
+	}
+
+	kthfromend(k) {
+		let current = this.head;
+		let length = 0;
+
+		while (current) {
+			length++;
+			current = current.next;
+		}
+
+		let index = 1;
+		current = this.head;
+
+		while (current && index !== length - k) {
+			index++;
+			current = current.next;
+		}
+
+		return current ? current : 'exception';
+	}
+
+	toString() {
+		let data = ''; //string to save data from our node
+		let current = this.header; //created current to start at the header
+		while (current !== null) {
+			// well keep going until current is equal to null if current is null then there are no nodes left in the list
+			data = data + current.data + ''; //saves the data at current node in a string
+			current = current.next; //now we go to the next node
+		}
+		return data; // now we return the data
+	}
 }
-module.exports = LinkedList;
 
-
+module.exports = List;
