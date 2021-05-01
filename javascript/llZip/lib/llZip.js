ad0b4282d5e59297e5ffa93a10d04ba5ee795d7d
Elijah Prom <elijahprom@gmail.com>
2021-05-01 11:08:50 -0700

    finished but no tests

--- javascript/llZip/lib/llZip.js
+++ javascript/llZip/lib/llZip.js
@@ -1,40 +1,32 @@
-/* eslint-disable indent */
+  
 'use strict';
 
-const LinkedList = require('../linkedList.js');
+const LinkedList = require('../linked-list.js');
 
-let zipLists = (list1, list2) => {
-	let newList = new LinkedList();
-	let currentList1 = list1.head;
-	let currentList2 = list2.head;
+module.exports = function zipLists(list1, list2){
 
-	while (currentList1 || currentList2) {
-		if (currentList1) {
-			newList.append(currentList1.value);
-			currentList1 = currentList1.next;
-		}
-		if (currentList2) {
-			newList.append(currentList2.value);
-			currentList2 = currentList2.next;
-		}
-	}
-	return newList;
+  const llZip = new LinkedList();
+
+  let current1 = list1.head;
+  let current2 = list2.head;
+
+  llZip.append(current1.value);
+  llZip.append(current2.value);
+
+  while(current1.next || current2.next) {
+
+    if(current1.next){
+      llZip.append(current1.next.value);
+      current1 = current1.next;
+    }
+
+    if(current2.next){
+      llZip.append(current2.next.value);
+      current2 = current2.next;
+    }
+
+  }
+
+  return llZip;
+
 };
-
-module.exports = zipLists;
-
-let input1 = new LinkedList();
-input1.append(1);
-input1.append(3);
-input1.append(5);
-input1.append(7);
-
-let input2 = new LinkedList();
-input2.append(2);
-input2.append(4);
-input2.append(6);
-input2.append(8);
-
-let check = zipLists(input1, input2);
-
-console.log(check);
